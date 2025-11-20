3a1cd619c63ce9c2a549e6936f82c9a6c87dbe88
Your Name <you@example.com>
2025-11-20 12:46:03 +0330

    add ui component

--- Assets/Scripts/ApiConfig.cs
+++ Assets/Scripts/ApiConfig.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ApiConfig", menuName = "TicTacThrow/Api Config", order = 0)]
 public class ApiConfig : ScriptableObject
 {
-    [SerializeField] private string baseUrl = "http://localhost:3000";
+    [SerializeField] private string baseUrl = "http://172.24.150.11:3000";
 
     public string BaseUrl => baseUrl;
 
